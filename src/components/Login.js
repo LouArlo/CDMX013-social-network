ec0abf9854ff8c490510e91a404644de7ec93450
LouArlo <lulu.arlo1306@gmail.com>
2022-09-09 10:08:23 -0500

    implementación SPA

---
+++ src/components/Login.js
@@ -0,0 +1,24 @@
+import { onNavigate } from "../main.js";
+
+export const Login = () =>{
+    const div = document.createElement('div');
+    const title = document.createElement('h2');
+    const button = document.createElement('button');
+    const buttonback = document.createElement('button');
+    const InputEmail = document.createElement('input');
+    const inputPass = document.createElement('input');
+
+    button.textContent= 'Entrar';
+    buttonback.textContent= 'Regresa';
+    title.textContent = "Inicia Sesión"
+
+    button.addEventListener('click', () => {
+        onNavigate('/');
+    });
+    buttonback.addEventListener('click', () => {
+        onNavigate('/');
+    });
+    div.append(title, InputEmail, inputPass, button, buttonback);
+
+    return div;
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,50 @@
+import {Welcome } from './components/Welcome.js'
+import {Login } from './components/Login.js';
+import {Register} from  './components/Register.js';
+
+const root = document.getElementById('root');
+
+const routes = {
+    '/': Welcome,
+    '/Login' : Login,
+    '/Register' : Register,
+};
+
+export const onNavigate = (pathname) => {
+window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname,
+);
+root.removeChild(root.firstChild);
+
+root.appendChild(routes[pathname]());
+};
+
+const component = routes[window.location.pathname];
+
+window.onpopstate = () => {
+    root.removeChild(root.firstChild);
+
+    root.append(component());
+};
+
+root.appendChild(component());
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+//import { myFunction } from './lib/index.js';
 
-myFunction();
+//myFunction();
